6dc215c482526681fd238302cfbb3f848346f3b7
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-04-21 19:48:33 +0800

    Opposite number

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-// You get an array of numbers, return the sum of all of the positives ones.
-// Example [1,-4,7,12] => 1 + 7 + 12 = 20
-// Note: if there is nothing to sum, the sum is default to 0.
-function positiveSum(arr) {
-    let sum = 0;
-    arr.filter((i) => i > 0).forEach((i) => (sum += i));
-    return sum;
+// Very simple, given a number, find its opposite.
+//
+// Examples:
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+    return -number;
 }
-console.log(positiveSum([1, -2, 3, 4, 5]));
+
+console.log(opposite(-5));
