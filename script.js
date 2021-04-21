1cdde5f078b98af71ab433d41bdc5ea4dee6d158
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-04-21 19:52:57 +0800

    Return Negative

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-// Very simple, given a number, find its opposite.
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
 //
-// Examples:
-// 1: -1
-// 14: -14
-// -34: 34
+// Example:
+// makeNegative(1); // return -1
+// makeNegative(-5); // return -5
+// makeNegative(0); // return 0
+// makeNegative(0.12); // return -0.12
+// Notes:
+//
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
 
-function opposite(number) {
-    return -number;
+function makeNegative(num) {
+    return num > 0 ? -num : num;
 }
 
-console.log(opposite(-5));
+console.log(makeNegative(-5));
