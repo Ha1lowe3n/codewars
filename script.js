3a7558e26c1c33e687f2057e1714087cc1b8b8c6
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-04-24 00:46:21 +0800

    Descending Order

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
 //
-// Example:
-//
-// highAndLow("1 2 3 4 5");  // return "5 1"
-// highAndLow("1 2 -3 4 5"); // return "5 -3"
-// highAndLow("1 9 3 4 -5"); // return "9 -5"
-// Notes:
-//
-// All numbers are valid Int32, no need to validate them.
-// There will always be at least one number in the input string.
-// Output string must be two numbers separated by a single space, and highest number is first.
+// Examples:
+// Input: 42145 Output: 54421
+// Input: 145263 Output: 654321
 
-function highAndLow(numbers) {
-    const arr = numbers.split(" ");
-    return `${Math.max(...arr)} ${Math.min(...arr)}`;
+function descendingOrder(n) {
+    return +n
+        .toString()
+        .split("")
+        .sort((a, b) => b - a)
+        .join("");
 }
 
-console.log(highAndLow("1 2 3"));
+console.log(descendingOrder("6456263045642"));
