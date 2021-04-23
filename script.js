93bc7848ba86b6ca9ee7e36da0989fb0687ced03
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-04-24 00:55:18 +0800

    Square Every Digit

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
-//
-// Examples:
-// Input: 42145 Output: 54421
-// Input: 145263 Output: 654321
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+// Note: The function accepts an integer and returns an integer
 
-function descendingOrder(n) {
-    return +n
+function squareDigits(num) {
+    return +num
         .toString()
         .split("")
-        .sort((a, b) => b - a)
+        .map((n) => n * n)
         .join("");
 }
 
-console.log(descendingOrder("6456263045642"));
+console.log(squareDigits(9119));
