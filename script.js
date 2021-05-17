3f948b188f10f5af6f800c362ffaf3bb6b2afbdc
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-05-18 01:03:20 +0800

    6 kyu: Sum of Digits / Digital Root

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-// Given an array of integers, find the one that appears an odd number of times.
-// There will always be only one integer that appears an odd number of times.
+// Digital root is the recursive sum of all the digits in a number.
 
-function findOdd(A) {
-    return A.reduce((a, b) => a ^ b);
+// Given n, take the sum of the digits of n. If that value has more than one digit,
+// continue reducing in this way until a single-digit number is produced.
+// The input will be a non-negative integer.
+
+// Examples
+// 16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function digital_root(n) {
+    const arr = n.toString().split("");
+    let result = 0;
+
+    arr.map((n) => (result += +n));
+
+    const rec = () => {
+        if (result.toString().length > 1) {
+            const newArr = result.toString().split("");
+            result = 0;
+            newArr.map((n) => (result += +n));
+            rec();
+        }
+    };
+    rec();
+
+    return result;
 }
+
+console.log(digital_root(16));
+console.log(digital_root(942));
+console.log(digital_root(132189));
+console.log(digital_root(493193));
